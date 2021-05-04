48f04e1ad0f6675c382fc8cdaf78fe0be3f4a02e
screat100 <61044260+screat100@users.noreply.github.com>
2021-05-04 18:31:05 +0900

    test CVR4 Models

---
+++ Project/Assets/MyProject/CVR 4/Scripts/CVR4_Area.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class CVR4_Area : MonoBehaviour
+{
+    [System.Serializable]
+    public class AgentInfo
+    {
+        public CVR4_Agent agent;
+
+        [HideInInspector]
+        public Vector3 startPos;
+
+        [HideInInspector]
+        public Quaternion startRot;
+
+        [HideInInspector]
+        public Rigidbody rb;
+    }
+
+
+
+
+    [Header("Max Environment Steps")] public int MaxEnvironmentSteps = 1500; // 50 (per 1 second)
+
+    public List<AgentInfo> chaserList;
+    public List<AgentInfo> runnerList;
+
+    private SimpleMultiAgentGroup chaserGroup;
+    private SimpleMultiAgentGroup runnerGroup;
+
+    public float agentRunSpeed = 10f;
+    [HideInInspector]
+    public int m_ResetTimer;
+    [HideInInspector]
+    public int catchedRunnerNum;
+
+    [Header("Runner Random Spawn Position")]
+    public GameObject Leftup;
+    public GameObject Rightdown;
+
+    void Start()
+    {
+        chaserGroup = new SimpleMultiAgentGroup();
+        runnerGroup = new SimpleMultiAgentGroup();
+
+        foreach (var item in chaserList)
+        {
+            item.startPos = item.agent.transform.localPosition;
+            item.startRot = item.agent.transform.localRotation;
+            item.rb = item.agent.GetComponent<Rigidbody>();
+            chaserGroup.RegisterAgent(item.agent);
+        }
+        foreach (var item in runnerList)
+        {
+            item.startPos = item.agent.transform.localPosition;
+            item.startRot = item.agent.transform.localRotation;
+            item.rb = item.agent.GetComponent<Rigidbody>();
+            runnerGroup.RegisterAgent(item.agent);
+        }
+
+        m_ResetTimer = 0;
+        catchedRunnerNum = 0;
+        ResetScene();
+    }
+
+    private void FixedUpdate()
+    {
+        m_ResetTimer++;
+
+        // time penalty 
+        chaserGroup.AddGroupReward(-1.0f / MaxEnvironmentSteps);
+        runnerGroup.AddGroupReward(2.0f / MaxEnvironmentSteps);
+
+        // Time Over => Runner win!
+        if (m_ResetTimer > MaxEnvironmentSteps)
+        {
+            runnerGroup.GroupEpisodeInterrupted();
+            chaserGroup.GroupEpisodeInterrupted();
+            ResetScene();
+        }
+
+
+    }
+
+
+    // Runner가 잡힐 때마다 chaser 보상받음
+    // 반대로 runner는 처벌
+    public void RunnerIsCatched()
+    {
+        catchedRunnerNum++;
+        chaserGroup.AddGroupReward(2.0f / runnerList.Count);
+        runnerGroup.AddGroupReward(-2.0f / runnerList.Count);
+
+        // All runners are catched => chaser win!
+        if (catchedRunnerNum >= runnerList.Count)
+        {
+            runnerGroup.GroupEpisodeInterrupted();
+            chaserGroup.GroupEpisodeInterrupted();
+            ResetScene();
+        }
+
+    }
+
+
+
+
+
+    void ResetScene()
+    {
+
+        foreach (var item in chaserList)
+        {
+            item.agent.transform.localPosition = new Vector3(
+                Random.Range(Leftup.transform.localPosition.x, Rightdown.transform.localPosition.x),
+                0,
+                Random.Range(Leftup.transform.localPosition.z, Rightdown.transform.localPosition.z));
+            item.agent.transform.localRotation = item.startRot;
+            item.rb.velocity = Vector3.zero;
+            item.rb.angularVelocity = Vector3.zero;
+        }
+
+        foreach (var item in runnerList)
+        {
+            if (!item.agent.gameObject.activeInHierarchy)
+            {
+                item.agent.gameObject.SetActive(true);
+                runnerGroup.RegisterAgent(item.agent);
+            }
+
+            item.agent.transform.localPosition = new Vector3(
+                Random.Range(Leftup.transform.localPosition.x, Rightdown.transform.localPosition.x),
+                0,
+                Random.Range(Leftup.transform.localPosition.z, Rightdown.transform.localPosition.z));
+            item.agent.transform.localRotation = item.startRot;
+            item.rb.velocity = Vector3.zero;
+            item.rb.angularVelocity = Vector3.zero;
+        }
+
+        m_ResetTimer = 0;
+        catchedRunnerNum = 0;
+    }
+
+
+}
